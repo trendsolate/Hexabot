238cd5fdf169781704d60070422ec16d56c198f0
xNoerPlays <97339279+xNoerPlaysCodes@users.noreply.github.com>
2023-09-22 16:01:14 +0530

    Added commands like;
    Noer:
    - Added ?verify_setup
    - Added proper help menu
    - Added ping command returning client latency
    
    Signed-off-by: xNoerPlays <97339279+xNoerPlaysCodes@users.noreply.github.com>

--- hexamain.py
+++ hexamain.py
@@ -1,31 +1,29 @@
-import discord
+import discord, time, platform, asyncio, os, TOKEN
 from discord import app_commands
-from discord.ext import commands, tasks 
+from discord.ext import commands, tasks
 from itertools import cycle
 from colorama import Back, Fore, Style
-import time
-import platform
-
-import os 
-import TOKEN
 from TOKEN import TOKEN
+from config import PREFIX, color
 
 intents = discord.Intents.all()
 intents.message_content = True
 
-client = commands.Bot(command_prefix="?" , intents=discord.Intents.all())
+# Change prefix in config.py
+client = commands.Bot(command_prefix=PREFIX , intents=intents, help_command=None) # Help command is custom
+bot_status = cycle([f"Made by Trendsolate", "From The HEX Network", f"Ready to help '{PREFIX}'", "Reading a book", "Waiting for requests", "Playing Fortnite"])
 
-bot_status = cycle(["Made by Trendsolate", "From The HEX Network", "Ready to help '?'", "Reading a book", "Waiting for requests", "Playing Fortnite"])
-
+# Continuously changes presence activity every 30 seconds to the ones in bot_status
 @tasks.loop(seconds=30)
 async def change_status():
     await client.change_presence(activity=discord.Game(next(bot_status)))
 
+# When bot is ready and logged in:
 @client.event
 async def on_ready():
     # this runs when the account is logged into.
     prfx = (Back.BLACK + Fore.GREEN + time.strftime("%H:%M:%S UTC", time.gmtime()) + Back.RESET + Fore.WHITE + Style.BRIGHT)
-    print("Hexa at your service and logged in as {0.user}".format(client))
+    print(f"🚀 Logged in as {client.user} | HexaBot")
     change_status.start()
     try:
         synced = await client.tree.sync()
@@ -33,12 +31,53 @@ async def on_ready():
 
     except Exception as e:
         print(e) 
-    # this is the code for commands and conversation.
+    client.add_view(Verification())
+
+# This is the code for commands
 @client.command()
-async def hello(ctx):
-    await ctx.send("hey wsg!")
+async def help(ctx):
+    helpmenu = discord.Embed(
+        title='Help Menu',
+        description=f'''
+{PREFIX}help - This!
+{PREFIX}ping - Returns client latency
+{PREFIX}verify_setup - Sets up verification
+''',
+    color=discord.Color(color)
+    )
+    helpmenu.set_footer(text='Hexabot | [PLACEHOLDER TEXT]')
+    await ctx.send(embed=helpmenu)
+
+@client.command()
+async def ping(ctx):
+    og_msg = await ctx.send(f"Calculating....")
+    await asyncio.sleep(1)
+    await og_msg.edit(content=f"Pong! {round(client.latency * 1000)}ms")
 
 
+# !! DO NOT TOUCH !! THIS IS CODE FOR VERIFICATION
 
+class Verification(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout = None)
+    @discord.ui.button(label="Verify", custom_id="Verify", style=discord.ButtonStyle.success)
+    async def verify(self, interaction, button):
+        role = 1144021740331225098
+        user = interaction.user
+        if role not in [y.id for y in user.roles]:
+            await user.add_roles(user.guild.get_role(role))
+            await user.send(f"You've been verified in {ctx.guild.name}!")
 
+@client.command()
+async def verify_setup(ctx):
+    embed = discord.Embed(
+        title='Verification',
+        description='Use the menu below to verify yourself to get access to the server! 👇✅',
+    color=discord.Color(color)
+    )
+    embed.set_footer(text=f'{ctx.guild.name} • Verification')
+
+    await ctx.send(embed=embed, view=Verification())
+
+# Run the bot using the provided token
 client.run(TOKEN)
