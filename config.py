8f2eed2bec209324b8ae4fcb0d03feed9b0e37d7
xNoerPlays <97339279+xNoerPlaysCodes@users.noreply.github.com>
2023-09-22 16:06:58 +0530

    added devs
    Signed-off-by: xNoerPlays <97339279+xNoerPlaysCodes@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,2 +1,11 @@
 PREFIX = '?'
-color = '2383D0'
+color = int('2383D0', 16)
+devs = [
+    1044817642143371364, # noerlol
+    1095310535140057109, # trendsolate
+    885036358467481601, # sexy sanskrit guy
+    825330059887509536, # bunkyeister
+    805817228498436099, # admi
+    1126175772546175036, # crystal
+    587204069408636929, # exotic.js
+]
